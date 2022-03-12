ecdf06367b1850b885e31237df4ae162697e9816
MikiMirai <ased223@abv.bg>
2022-03-12 17:20:12 +0200

    Created BaseController and added authorize.

---
+++ ASPNET-WebApp/Controllers/BaseController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASPNET_WebApp.Controllers
+{
+    [Authorize]
+    public class BaseController : Controller
+    {
+
+    }
+}
--- ASPNET-WebApp/Controllers/HomeController.cs
+++ ASPNET-WebApp/Controllers/HomeController.cs
@@ -5,7 +5,7 @@ using System.Diagnostics;
 
 namespace ASPNET_WebApp.Controllers
 {
-    public class HomeController : Controller
+    public class HomeController : BaseController
     {
         private readonly ILogger<HomeController> _logger;
 

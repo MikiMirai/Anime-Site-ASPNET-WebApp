7ee4a24782fa426407c533e1339ee5954b1ee351
MikiMirai <ased223@abv.bg>
2022-04-27 03:48:55 +0300

    Validation constants added.

---
+++ ASPNET-WebApp.Core/Constants/DataValidationConstants.cs
@@ -0,0 +1,19 @@
+﻿namespace ASPNET_WebApp.Core.Constants
+{
+    public static class DataValidationConstants
+    {
+        public const string MustBeBetween = "{0} must be between {2} and {1} characters.";
+
+        public const int AnimeNameMaxLength = 120;
+        public const int AnimeNameMinLength = 8;
+
+        public const int ImageUrlMaxLength = 250;
+        public const int ImageUrlMinLength = 20;
+
+        public const int DescriptionAnimeMaxLength = 2000;
+        public const int DescriptionAnimeMinLength = 20;
+
+        public const int MaxLength200 = 200;
+        public const int MinLength10 = 10;
+    }
+}

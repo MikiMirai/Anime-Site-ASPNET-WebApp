4cd32b140898fa50ad136def7509afeffd4ac813
MikiMirai <ased223@abv.bg>
2022-04-29 22:51:30 +0300

    Summary for testing added.

--- ASPNET-WebApp.Core/Services/AnimeService.cs
+++ ASPNET-WebApp.Core/Services/AnimeService.cs
@@ -19,122 +19,12 @@ namespace ASPNET_WebApp.Core.Services
             dbContext = _dbContext;
         }
 
-        public async Task<IEnumerable<AnimeListViewModel>> GetAnimes()
-        {
-            var animes = repo.All<Anime>();
-
-            var animeView = await animes.Select(anime => new AnimeListViewModel()
-            {
-                Id = anime.Id,
-                Name = anime.Name,
-                Image = anime.Image,
-                Aired = anime.Aired,
-                Episodes = anime.Episodes,
-                Status = anime.Status,
-                ReviewCount = anime.Reviews.Count,
-            })
-                .OrderBy(a => a.Name)
-                .ToListAsync();
-
-            return animeView;
-        }
-
-        public async Task<IEnumerable<AnimeListViewModel>> GetAnimesTrending()
-        {
-            var animes = repo.All<Anime>();
-
-            var animeView = await animes.Select(anime => new AnimeListViewModel()
-            {
-                Id = anime.Id,
-                Name = anime.Name,
-                Image = anime.Image,
-                Aired = anime.Aired,
-                Episodes = anime.Episodes,
-                Status = anime.Status,
-                ReviewCount = anime.Reviews.Count,
-            })
-                .OrderByDescending(a => a.ReviewCount)
-                .ToListAsync();
-
-            return animeView;
-        }
-
-        public async Task<Anime> GetAnimeById(string id)
-        {
-            var anime = await dbContext.Animes
-                .Where(x => x.Id == id)
-                .Include(x => x.Genres)
-                .FirstOrDefaultAsync();
-
-            if (anime == null)
-            {
-                throw new ArgumentNullException($"The anime with id:{id} does not exist.");
-            }
-
-            return anime;
-        }
-
-        public async Task<Anime> GetAnimeForEdit(string id)
-        {
-            var anime = dbContext.Animes.Where(x => x.Id == id)
-                .Include(x => x.Genres)
-                .FirstOrDefault();
-
-            if (anime == null)
-            {
-                throw new ArgumentNullException(nameof(anime));
-            }
-
-            var foundAnime = new Anime()
-            {
-                Id = anime.Id,
-                Name = anime.Name,
-                Image = anime.Image,
-                Aired = anime.Aired,
-                Episodes = anime.Episodes,
-                Status = anime.Status,
-                Description = anime.Description,
-                Studios = anime.Studios,
-                Producers = anime.Producers,
-                Genres = anime.Genres,
-                Duration = anime.Duration,
-                Rating = anime.Rating
-            };
-
-            return foundAnime;
-        }
-
-        public async Task<bool> UpdateAnime(AnimeEditViewModel model)
-        {
-            bool result = false;
-
-            var anime = await repo.GetByIdAsync<Anime>(model.Id);
-
-            if (anime != null)
-            {
-                anime.Name = model.Name;
-                anime.Image = model.Image;
-                anime.Aired = model.Aired;
-                anime.Episodes = model.Episodes;
-                anime.Status = model.Status;
-                anime.Description = model.Description;
-                anime.Studios = model.Studios;
-                anime.Producers = model.Producers;
-                anime.Genres = model.Genres;
-                anime.Duration = model.Duration;
-                anime.Rating = model.Rating;
-
-                await repo.SaveChangesAsync();
-                result = true;
-            }
-			else
-			{
-                throw new ArgumentNullException("The edit model is null.");
-            }
-
-            return result;
-        }
-
+        /// <summary>
+        /// Create tested, AlreadyExists tested.
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentNullException"></exception>
         public async Task<bool> CreateAnime(AnimeCreateViewModel model)
         {
             if (await AnimeAlreadyExistInCollection(model.Name))
@@ -168,16 +58,146 @@ namespace ASPNET_WebApp.Core.Services
             return true;
         }
 
-        public async Task<bool> AnimeAlreadyExistInCollection(string animeName)
+        /// <summary>
+        /// Return animes tested.
+        /// </summary>
+        /// <returns></returns>
+        public async Task<IEnumerable<AnimeListViewModel>> GetAnimes()
         {
-            var anime = await dbContext.Animes.FirstOrDefaultAsync(x => x.Name == animeName);
+            var animes = repo.All<Anime>();
+
+            var animeView = await animes.Select(anime => new AnimeListViewModel()
+            {
+                Id = anime.Id,
+                Name = anime.Name,
+                Image = anime.Image,
+                Aired = anime.Aired,
+                Episodes = anime.Episodes,
+                Status = anime.Status,
+                ReviewCount = anime.Reviews.Count,
+            })
+                .OrderBy(a => a.Name)
+                .ToListAsync();
+
+            return animeView;
+        }
+
+        /// <summary>
+        /// Returns animes tested.
+        /// </summary>
+        /// <returns></returns>
+        public async Task<IEnumerable<AnimeListViewModel>> GetAnimesTrending()
+        {
+            var animes = repo.All<Anime>();
+
+            var animeView = await animes.Select(anime => new AnimeListViewModel()
+            {
+                Id = anime.Id,
+                Name = anime.Name,
+                Image = anime.Image,
+                Aired = anime.Aired,
+                Episodes = anime.Episodes,
+                Status = anime.Status,
+                ReviewCount = anime.Reviews.Count,
+            })
+                .OrderByDescending(a => a.ReviewCount)
+                .ToListAsync();
+
+            return animeView;
+        }
+
+        /// <summary>
+        /// Return anime tested, return exception tested.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentNullException"></exception>
+        public async Task<Anime> GetAnimeById(string id)
+        {
+            var anime = await dbContext.Animes
+                .Where(x => x.Id == id)
+                .Include(x => x.Genres)
+                .FirstOrDefaultAsync();
 
             if (anime == null)
             {
-                return false;
+                throw new ArgumentNullException($"The anime with id:{id} does not exist.");
             }
 
-            return true;
+            return anime;
+        }
+
+        /// <summary>
+        /// Return anime tested, return exception tested.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentNullException"></exception>
+        public async Task<Anime> GetAnimeForEdit(string id)
+        {
+            var anime = await dbContext.Animes.Where(x => x.Id == id)
+                .Include(x => x.Genres)
+                .FirstOrDefaultAsync();
+
+            if (anime == null)
+            {
+                throw new ArgumentNullException(nameof(anime));
+            }
+
+            var foundAnime = new Anime()
+            {
+                Id = anime.Id,
+                Name = anime.Name,
+                Image = anime.Image,
+                Aired = anime.Aired,
+                Episodes = anime.Episodes,
+                Status = anime.Status,
+                Description = anime.Description,
+                Studios = anime.Studios,
+                Producers = anime.Producers,
+                Genres = anime.Genres,
+                Duration = anime.Duration,
+                Rating = anime.Rating
+            };
+
+            return foundAnime;
+        }
+
+        /// <summary>
+        /// Returns false tested, updates anime tested.
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentNullException"></exception>
+        public async Task<bool> UpdateAnime(AnimeEditViewModel model)
+        {
+            bool result = false;
+
+            var anime = await repo.GetByIdAsync<Anime>(model.Id);
+
+            if (anime != null)
+            {
+                anime.Name = model.Name;
+                anime.Image = model.Image;
+                anime.Aired = model.Aired;
+                anime.Episodes = model.Episodes;
+                anime.Status = model.Status;
+                anime.Description = model.Description;
+                anime.Studios = model.Studios;
+                anime.Producers = model.Producers;
+                anime.Genres = model.Genres;
+                anime.Duration = model.Duration;
+                anime.Rating = model.Rating;
+
+                await repo.SaveChangesAsync();
+                result = true;
+            }
+			else
+			{
+                throw new ArgumentNullException("The edit model is null.");
+            }
+
+            return result;
         }
 
         public async Task<AnimeDetailsViewModel> GetAnimeDetailsById(string id)
@@ -221,6 +241,21 @@ namespace ASPNET_WebApp.Core.Services
             return foundAnime;
         }
 
-        
+        /// <summary>
+        /// Returns true tested, returns false if anime donesn't exist tested.
+        /// </summary>
+        /// <param name="animeName"></param>
+        /// <returns></returns>
+        public async Task<bool> AnimeAlreadyExistInCollection(string animeName)
+        {
+            var anime = await dbContext.Animes.FirstOrDefaultAsync(x => x.Name == animeName);
+
+            if (anime == null)
+            {
+                return false;
+            }
+
+            return true;
+        }
     }
 }

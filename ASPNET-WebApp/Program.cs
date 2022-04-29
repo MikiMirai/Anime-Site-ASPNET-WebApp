dda1b5f5ababcff56e1cdf923cb8ea080a1ada06
MikiMirai <ased223@abv.bg>
2022-04-29 14:52:54 +0300

    Ordered the code a bit.

--- ASPNET-WebApp/Program.cs
+++ ASPNET-WebApp/Program.cs
@@ -4,55 +4,63 @@ using ASPNET_WebApp.Infrastructure.Data.Identity;
 using ASPNET_WebApp.ModelBinders;
 using Microsoft.AspNetCore.Identity;
 
-var builder = WebApplication.CreateBuilder(args);
-// Add services to the container.
-builder.Services.AddApplicationDbContexts(builder.Configuration);
-
-builder.Services.AddDefaultIdentity<ApplicationUser>(options =>
+public class Program
 {
-    options.SignIn.RequireConfirmedAccount = true;
-})
-    .AddRoles<IdentityRole>()
-    .AddEntityFrameworkStores<ApplicationDbContext>();
-
-builder.Services.AddControllersWithViews()
-    .AddMvcOptions(options =>
+    public static void Main(string[] args)
     {
-        options.ModelBinderProviders.Insert(0, new DecimalModelBinderProvider());
-        options.ModelBinderProviders.Insert(1, new DateTimeModelBinderProvider(FormattingConstants.NormalDateFormat));
-        options.ModelBinderProviders.Insert(2, new DoubleModelBinderProvider());
-    });
+        var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddApplicationServices();
+        builder.Services.AddApplicationDbContexts(builder.Configuration);
 
-var app = builder.Build();
+        builder.Services.AddDefaultIdentity<ApplicationUser>(options =>
+        {
+            options.SignIn.RequireConfirmedAccount = true;
+        })
+            .AddRoles<IdentityRole>()
+            .AddEntityFrameworkStores<ApplicationDbContext>();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseMigrationsEndPoint();
+        builder.Services.AddControllersWithViews()
+            .AddMvcOptions(options =>
+            {
+                options.ModelBinderProviders.Insert(0, new DecimalModelBinderProvider());
+                options.ModelBinderProviders.Insert(1, new DateTimeModelBinderProvider(FormattingConstants.NormalDateFormat));
+                options.ModelBinderProviders.Insert(2, new DoubleModelBinderProvider());
+            });
+
+        // Add services to the container.
+        builder.Services.AddApplicationServices();
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseMigrationsEndPoint();
+        }
+        else
+        {
+            app.UseExceptionHandler("/Home/Error");
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
+        }
+
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+
+        app.UseRouting();
+
+        app.UseAuthentication();
+        app.UseAuthorization();
+
+        app.MapControllerRoute(
+            name: "Area",
+            pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+        app.MapControllerRoute(
+            name: "default",
+            pattern: "{controller=Home}/{action=Index}/{id?}");
+        app.MapRazorPages();
+
+        app.Run();
+    }
 }
-else
-{
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
 
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseAuthentication();
-app.UseAuthorization();
-
-app.MapControllerRoute(
-    name: "Area",
-    pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
-app.MapRazorPages();
-
-app.Run();
